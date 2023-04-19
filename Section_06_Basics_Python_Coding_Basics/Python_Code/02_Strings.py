32dbcdfbb21291559b323c5752972b0f357254bc
Romulo Vieira <romulo.vieira777@gmail.com>
2023-04-19 16:03:53 -0300

    Basics - Python Coding Basics

--- Section_06_Basics_Python_Coding_Basics/Python_Code/02_Strings.py
+++ Section_06_Basics_Python_Coding_Basics/Python_Code/02_Strings.py
@@ -1,10 +1,11 @@
 First_name = "Sara"
 print(First_name)
-# -----------------------------
+# --------------------------------
 
 First_name = "Mj"
 print(f"My name is {First_name}.")
-# -----------------------------
+# --------------------------------
 
 First_name = "24424"
 print(f"My name is {First_name}.")
+# --------------------------------
---
+++ Section_06_Basics_Python_Coding_Basics/Python_Code/06_Variables.py
@@ -0,0 +1,79 @@
+x = 'Hi'
+print(x)
+# --------------------------------
+
+a = 'Hello'
+print(a)
+# --------------------------------
+
+_FirstName = 'Sara'
+print(_FirstName)
+# --------------------------------
+
+FirstName = 'Sara'
+print(FirstName)
+# --------------------------------
+
+My_Var = 'Great!'
+print(My_Var)
+# --------------------------------
+
+My_var2 = 'Awesome!'
+print(My_var2)
+# --------------------------------
+
+Age = 12
+print(Age)
+# --------------------------------
+
+age = 120
+print(age)
+# --------------------------------
+
+AGE = 122
+print(AGE)
+# --------------------------------
+
+myFristName = 'Sara'
+print(myFristName)
+# --------------------------------
+
+iLovePython = True
+print(iLovePython)
+# --------------------------------
+
+MyFirstName = 'Sara'
+print(MyFirstName)
+# --------------------------------
+
+My_First_Name = 'Sara'
+print(My_First_Name)
+# --------------------------------
+
+a = 'Sara'
+b = 'Mj'
+c = 'Mary'
+print(a)
+print(b)
+print(c)
+# --------------------------------
+
+a, b, c = 'Sara', 'Mj', 'Mary'
+print(a)
+print(b)
+print(c)
+# --------------------------------
+
+a = 'Mary'
+b = 'Mary'
+c = 'Mary'
+print(a)
+print(b)
+print(c)
+# --------------------------------
+
+a = b = c = 'Sara'
+print(a)
+print(b)
+print(c)
+# --------------------------------
