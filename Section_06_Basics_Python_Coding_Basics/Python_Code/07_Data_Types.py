7673c721f25dc206ff4e659360494555fa86d35b
Romulo Vieira <romulo.vieira777@gmail.com>
2023-04-20 16:34:01 -0300

    Basics - Python Coding Basics

---
+++ Section_06_Basics_Python_Coding_Basics/Python_Code/07_Data_Types.py
@@ -0,0 +1,19 @@
+color = "Red"
+print(type(color))
+print(f"Type is:", type({color}))
+# ----------------------------------------
+
+salary = 250000000
+print(type(salary))
+print(f"Type of this is:", type({salary}))
+# ----------------------------------------
+
+g = 536.44444
+print(type(g))
+print(f"Type of this is:", type({g}))
+# ----------------------------------------
+
+z = True
+print(type(z))
+print(f"Type of this is:", type({z}))
+# ----------------------------------------
