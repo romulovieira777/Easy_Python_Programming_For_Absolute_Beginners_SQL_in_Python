b5aab3b4a6478f41d20fb0c5decd9168f031abf4
Romulo Vieira <romulo.vieira777@gmail.com>
2023-04-28 19:53:11 -0300

    Basics - Strings in Python

---
+++ Section_07_Basics_Strings_in_Python/Python_Code/01_Working_Strings.py
@@ -0,0 +1,30 @@
+print("Hello World!")
+print('Hello World!')
+# ---------------------------------
+
+fn = "Abdullah"
+ln = "Musavi"
+
+print("Abdullah" + " " + "Musavi")
+print(fn + " " + ln)
+# ---------------------------------
+
+b = "Hi, my nais is \"Musavi\""
+print(b)
+# ---------------------------------
+
+c = 'Hi, my nais is "Musavi"'
+print(c)
+# ---------------------------------
+
+d = 'Hi, my nais is \'Musavi\''
+print(d)
+# ---------------------------------
+
+m = """First line
+Second line
+Third line
+Anything you want
+"""
+print(m)
+# ---------------------------------
