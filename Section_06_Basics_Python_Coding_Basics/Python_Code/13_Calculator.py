22b86d6b5f7f05b3ced8065f981d83f5d7330998
Romulo Vieira <romulo.vieira777@gmail.com>
2023-04-28 17:13:39 -0300

    Basics - Python Coding Basics

---
+++ Section_06_Basics_Python_Coding_Basics/Python_Code/13_Calculator.py
@@ -0,0 +1,12 @@
+fn = input("Enter the First Number: ")
+sn = input("Enter the Second Number: ")
+# ---------------------------------------------
+
+print(f"Type of first number is: {type(fn)}")
+print(f"Type of second number is: {type(sn)}")
+# ---------------------------------------------
+
+result = float(fn) + float(sn)
+# ---------------------------------------------
+
+print(f"Result is: {result}")
