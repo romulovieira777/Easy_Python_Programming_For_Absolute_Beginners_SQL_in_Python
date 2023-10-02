692560138a40e15b6862305540c012f235d966f5
Romulo Vieira <romulo.vieira777@gmail.com>
2023-10-02 16:31:56 -0300

    Advance - Modules in Python

---
+++ Section_09_Advance_Modules_in_Python/Python_Code/01_First_Module.py
@@ -0,0 +1,20 @@
+import math
+
+print(math.pi)
+# ------------------------------
+import math as MyMath
+
+print(MyMath.pi)
+# ------------------------------
+import math as mm
+
+print(mm.pi)
+# ------------------------------
+from math import pi
+
+print(pi)
+# ------------------------------
+from math import pi as MyPi
+
+print(MyPi)
+# ------------------------------
