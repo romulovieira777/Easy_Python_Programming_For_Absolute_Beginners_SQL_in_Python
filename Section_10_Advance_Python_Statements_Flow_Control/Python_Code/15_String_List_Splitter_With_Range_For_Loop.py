4a61267ba329e748d0a104ab79aa3fb2a9b261a0
RomuloVieira777 <romulo.vieira777@gmail.com>
2025-09-09 18:19:43 -0300

    Advance - Python Statements - Flow Control

---
+++ Section_10_Advance_Python_Statements_Flow_Control/Python_Code/15_String_List_Splitter_With_Range_For_Loop.py
@@ -0,0 +1,16 @@
+a = "Hello Python!"
+
+IndexList = range(0, 13)
+
+for index in IndexList:
+    r = a[index]
+    print(r)
+# ------------------------------------ #
+b = "Hello Python!"
+
+IndexList = range(0, len(b))
+
+for index in IndexList:
+    r = b[index]
+    print(r)
+# ------------------------------------ #
